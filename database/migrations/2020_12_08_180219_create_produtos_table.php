efabc4540affdc95ee726056a2d12ce1f0dfa0d4
Renato Mateus <renatomateusx@gmail.com>
2020-12-08 18:07:02 -0300

    Initialize

---
+++ database/migrations/2020_12_08_180219_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();            
+            $table->string('uuid')->unique();
+            $table->timestamp('dtc_creation')->nullable();
+            $table->string('nome');
+            $table->string('tipo');   
+            $table->timestamps();         
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
